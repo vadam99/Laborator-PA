fa0fc9297ecd060cb13c0418039a3999bbbd26f9
vadam99 <61696552+vadam99@users.noreply.github.com>
2020-04-30 22:04:24 +0300

    Add files via upload

---
+++ Laborator10/serverapp/src/serverapp/GameServer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverapp;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author vadam
+ */
+public class GameServer {
+    // Define the port on which the server is listening
+
+    public static final int PORT = 5030;
+
+    public GameServer() throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(PORT);
+            while (true) {
+                System.out.println("Waiting for a client ...");
+                Socket socket = serverSocket.accept();
+                new ClientThread(socket).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Ooops... " + e);
+        } finally {
+            serverSocket.close();
+        }
+    }
+}
